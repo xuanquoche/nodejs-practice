fd515aa97ece2d597a376e631fc53e2652ceff37
xuanquoche <nguyentrinhxuanquoc2003@gmail.com>
2024-11-25 21:38:21 +0700

    Error handler in api

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -4,13 +4,7 @@ const AccessService = require("../services/access.service");
 
 class AccessController {
   signUp = async (req, res, next) => {
-    const data = req.body;
-    try {
-      console.log(`[P]::signUp::`, data);
-      return res.status(201).json(await AccessService.signUp(req.body));
-    } catch (error) {
-      next(error);
-    }
+    return res.status(201).json(await AccessService.signUp(req.body));
   };
 }
 
--- src/services/access.service.js
+++ src/services/access.service.js
@@ -6,6 +6,7 @@ const crypto = require("node:crypto");
 const KeyTokenService = require("./keyToken.service");
 const { createTokenPair } = require("../auth/authUtils");
 const { getInfoData } = require("../utils");
+const { BadRequestError } = require("../core/error.response");
 
 const RoleShop = {
   SHOP: "SHOP",
@@ -21,10 +22,7 @@ class AccessService {
       const holderShop = await shopModel.findOne({ email });
 
       if (holderShop) {
-        return {
-          code: "xxxx",
-          message: "Shop already register",
-        };
+        throw new BadRequestError("Error: Shop already registered");
       }
 
       const passwordHash = await bcrypt.hash(password, 10);
