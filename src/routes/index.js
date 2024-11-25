16b91e5b6b018c6f3848be900c97cdcf9e2dfd4d
xuanquoche <nguyentrinhxuanquoc2003@gmail.com>
2024-11-25 21:00:53 +0700

    custom dynamic middleware for apikey and permission

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 "use strict";
 
 const express = require("express");
+const { apiKey, permission } = require("../auth/checkAuth");
 const router = express.Router();
 
-// router.get("", (req, res, next) => {
-//   return res.status(200).json({
-//     message: "welcome",
-//   });
-// });
+// check api key
+router.use(apiKey);
+router.use(permission("0000"));
+
+// check permission
 router.use("/v1/api", require("./access"));
 
 module.exports = router;
